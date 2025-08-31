370ab95e7bb2db1e9e18260ba69cc9f23e901732
NBhanuprakash2004 <bhanubhuvan1234@gmail.com>
2025-08-31 13:07:19 +0530

    Add Spring dependencies for validation, context, and testing; update component scanning in main application; modify server port and view prefix in application.yml

--- website/src/main/java/com/example/website/WebsiteApplication.java
+++ website/src/main/java/com/example/website/WebsiteApplication.java
@@ -2,7 +2,9 @@ package com.example.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.website.controller"+"com.example.resources")
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
